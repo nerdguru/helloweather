13730ba4d9b905a612199a66b8445fcd4da0354a
Pete Johnson <petecj2@gmail.com>
2017-03-20 10:14:13 -0700

    Added OpenWhiskLocal

---
+++ openWhiskLocal/handler.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var request = require('request');
+
+function main(params) {
+    var location = params.location || 'New Orleans, LA';
+    var url = 'https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json';
+
+    return new Promise(function(resolve, reject) {
+        request.get(url, function(error, response, body) {
+            if (error) {
+                reject(error);
+            }
+            else {
+                var condition = JSON.parse(body).query.results.channel.item.condition;
+                var text = condition.text;
+                var temperature = condition.temp;
+                var output = 'It is ' + temperature + ' degrees in ' + location + ' and ' + text;
+                resolve({msg: output});
+            }
+        });
+    });
+}
+
+exports.handler = main;
